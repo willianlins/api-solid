b1b55fd54704a034214649965f2359a4e0b42ca9
Willian Lins Araújo <willian.araujo@anchieta.br>
2024-09-20 08:12:56 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import fastify from 'fastify'
+import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
+
+export const app = fastify()
+
+app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validatio error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO Here we should log to do on external tool like Datadog/NewRelic
+  }
+
+  return reply.status(500).send({ messag: 'Internal server error' })
+})
